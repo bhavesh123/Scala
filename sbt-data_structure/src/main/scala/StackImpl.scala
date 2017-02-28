24d224642fd384398dc43f86d923ca74fd7c1545
Bhavesh Gadoya <bhavesh.gadoya89@gmail.com>
2017-02-28 22:48:44 +0530

    Update StackImpl.scala

--- sbt-data_structure/src/main/scala/StackImpl.scala
+++ sbt-data_structure/src/main/scala/StackImpl.scala
@@ -29,28 +29,17 @@ Constraints:
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import scala.collection.mutable.Stack;
 object StackImpl {
-
   var data = new Stack[Integer]();
-
-  def pop(): String = {
-    if (data.isEmpty) {
-      return "No Food"
-    }
-    var k = data.pop();
-    return k.toString;
-
-  }
-
-  def push(i: Integer): Unit = {
-    data.push(i)
-  }
-  /**
-   * @param args the command line arguments
-   */
+ 
   def main(args: Array[String]): Unit = {
     var br = new BufferedReader(new InputStreamReader(System.in));
     var line = br.readLine();
@@ -63,11 +52,17 @@ object StackImpl {
     for(i<-0 until inputs.length){
       var splitdata: Array[String] = inputs(i).split(" ");
       if (Integer.parseInt(splitdata(0)) == 1) {
-        println(pop);
+          if (data.isEmpty) {
+              println("No Food")
+          }
+          else{
+          var k = data.pop();
+          println(k);
+          }
       } else if (Integer.parseInt(splitdata(0)) == 2) {
         var value: Integer = Integer.parseInt(splitdata(1));
-        push(value)
-      }
+        data.push(value)
+      }    
     }
   }
 }
